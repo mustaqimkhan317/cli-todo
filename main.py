076b73bab15b72d3be82f4124be2e598ed4b01a2
Mustaqim Khan <mustaqimkhan317@gmail.com>
2025-12-21 16:31:16 -0500

    Added main.py as starting point

---
+++ main.py
@@ -0,0 +1,13 @@
+from todo import *
+from storage import *
+
+
+def main():
+    print("Welcome to the CLI To-do app\n")
+    todo = Todo() # Initialize the object
+    todo.display() # Display the list 
+    todo.take_input()
+    todo.display()
+
+if __name__ == "__main__":
+    main()
